730234591562b0562d91e0519e6547962ac5df43
Adrien Billard <adrienbillard@trakkeo.com>
2024-04-25 08:13:24 +0200

    ajout de favicon.ico

--- app/Views/footer.php
+++ app/Views/footer.php
@@ -1,6 +1,5 @@
 <footer class="bg-light text-center text-lg-start mt-auto">
     <div class="text-center p-3">
-        <?php echo 'Copyright : '.date("Y");?>
-        <a class="text-dark" href="#">Réalisation : Adrien</a>
+        <span style="color: #1abc9c;"><?php echo 'Copyright : '.date("Y");?> | Réalisation : Adrien</span>
     </div>
 </footer>
